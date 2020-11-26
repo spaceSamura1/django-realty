bdab092cb1804af42b00e56160ad89c23e93b338
techno-sapi3n <tyler.ceja5@gmail.com>
2020-11-25 17:24:48 -0800

    update ignore

--- btre/settings.py
+++ btre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from keys import setting_sec_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nc-mme!7k%ng!=2cbm@&cr$qg2e&)9vfuq!61rd$n6h6zd48j^'
+SECRET_KEY = setting_sec_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
